ee1459efbfb4f68ecfbf5948d7bd15eb2edf675c
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-03-09 09:29:14 +0100

    feat: add email

--- modules/features/apps/email.nix
+++ modules/features/apps/email.nix
@@ -29,6 +29,15 @@
               profiles.default = {
                 isDefault = true;
               };
+
+              settings = {
+                "ldap_2.servers.science.description" = "Radboud Science";
+                "ldap_2.servers.science.hostname" = "ldap.science.ru.nl";
+                "ldap_2.servers.science.port" = 389;
+                "ldap_2.servers.science.basedn" = "o=addressbook";
+                "ldap_2.servers.science.dirType" = 0;
+                "ldap_2.servers.science.filename" = "abook-science.sqlite";
+              };
             };
           }
         ];
--- modules/users/jorrit/configuration.nix
+++ modules/users/jorrit/configuration.nix
@@ -17,40 +17,70 @@
       "video"
     ];
 
-    extraHomeConfig.accounts.email.accounts."Radboud Science" = {
-      address = "jorrit.vanderheide@science.ru.nl";
-      realName = "Jorrit van der Heide";
-      primary = true;
+    extraHomeConfig = {
+      accounts.email.accounts."Radboud Outlook" = {
+        address = "jorrit.vanderheide@ru.nl";
+        passwordCommand = "cat /run/secrets/email_password_outlook";
+        primary = true;
+        realName = "Jorrit van der Heide";
+        userName = "jorrit.vanderheide@ru.nl";
 
-      imap = {
-        host = "post.science.ru.nl";
-        port = 993;
+        imap = {
+          host = "outlook.office365.com";
+          port = 993;
+        };
+
+        smtp = {
+          host = "smtp.office365.com";
+          port = 587;
+          tls.useStartTls = true;
+        };
+
+        thunderbird = {
+          enable = true;
+          settings = id: {
+            "mail.server.server_${id}.authMethod" = 10;
+            "mail.smtpserver.smtp_${id}.authMethod" = 10;
+          };
+        };
       };
 
-      smtp = {
-        host = "smtp.science.ru.nl";
-        port = 587;
-        tls.useStartTls = true;
+      accounts.email.accounts."Radboud Science" = {
+        address = "jorrit.vanderheide@science.ru.nl";
+        passwordCommand = "cat /run/secrets/email_password_science";
+        primary = false;
+        realName = "Jorrit van der Heide";
+        thunderbird.enable = true;
+        userName = "jvanderheide";
+
+        imap = {
+          host = "post.science.ru.nl";
+          port = 993;
+        };
+
+        smtp = {
+          host = "smtp.science.ru.nl";
+          port = 587;
+          tls.useStartTls = true;
+        };
       };
 
-      userName = "jvanderheide";
-      passwordCommand = "cat /run/secrets/email_password";
-    };
+      settings = {
+        git = {
+          signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVnWm8z0c77BQGCj65u0JMe6gcoEtGn+4yK4+CGMGHi";
+          userEmail = "jorrit+git@bw20.nl";
+          userName = "Jorrit van der Heide";
 
-    extraHomeConfig.settings = {
-      git = {
-        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVnWm8z0c77BQGCj65u0JMe6gcoEtGn+4yK4+CGMGHi";
-        userEmail = "jorrit+git@bw20.nl";
-        userName = "Jorrit van der Heide";
-
-        allowedSigningKeys = [
-          "jorrit+git@bw20.nl ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVnWm8z0c77BQGCj65u0JMe6gcoEtGn+4yK4+CGMGHi"
-        ];
+          allowedSigningKeys = [
+            "jorrit+git@bw20.nl ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVnWm8z0c77BQGCj65u0JMe6gcoEtGn+4yK4+CGMGHi"
+          ];
+        };
       };
     };
 
     userSecrets = {
-      email_password = { };
+      email_password_outlook = { };
+      email_password_science = { };
 
       user_password = {
         neededForUsers = true;
