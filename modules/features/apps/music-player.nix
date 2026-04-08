90e71ecf78daa69440301248844e07ae3440bd09
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-04-08 15:32:18 +0200

    feat: restore qobuz dektop entry

--- modules/features/apps/music-player.nix
+++ modules/features/apps/music-player.nix
@@ -1,4 +1,8 @@
 {
+  lib,
+  ...
+}:
+{
   flake.nixosModules.music-player =
     {
       pkgs,
@@ -15,6 +19,20 @@
             home.packages = with pkgs; [
               qobuz-player
             ];
+
+            xdg.desktopEntries.qobuz = {
+              comment = "Qobuz player";
+              exec = "${lib.getExe pkgs.ghostty} -e ${lib.getExe pkgs.qobuz-player}";
+              genericName = "Music player";
+              icon = "music-app";
+              name = "Qobuz";
+              type = "Application";
+
+              categories = [
+                "AudioVideo"
+                "Music"
+              ];
+            };
           }
         ];
       };
