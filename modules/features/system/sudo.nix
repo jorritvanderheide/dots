d47414230deedfad0ac4800d7e074ae08de70a3f
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-04-24 06:56:16 +0200

    fix(security): require sudo password, optional fingerprint auth

--- modules/features/system/sudo.nix
+++ modules/features/system/sudo.nix
@@ -1,10 +1,40 @@
 {
-  flake.nixosModules.sudo = {
-    config = {
-      security.sudo = {
-        execWheelOnly = true;
-        wheelNeedsPassword = false;
+  lib,
+  ...
+}:
+{
+  flake.nixosModules.sudo =
+    {
+      config,
+      ...
+    }:
+    let
+      cfg = config.my.sudo;
+    in
+    {
+      options.my.sudo = {
+        fingerprintAuth = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = ''
+            Allow fingerprint as an alternative to password for sudo.
+            Enables fprintd and registers pam_fprintd in the sudo PAM stack.
+          '';
+        };
+      };
+
+      config = {
+        security.sudo = {
+          execWheelOnly = true;
+          wheelNeedsPassword = true;
+        };
+
+        services.fprintd.enable = lib.mkIf cfg.fingerprintAuth true;
+        security.pam.services.sudo.fprintAuth = lib.mkIf cfg.fingerprintAuth true;
+
+        my.preservation.systemDirectories = lib.mkIf cfg.fingerprintAuth [
+          "/var/lib/fprint"
+        ];
       };
     };
-  };
 }
